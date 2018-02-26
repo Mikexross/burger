6b3c9f3d8406cc53166f02a2294b72ce4c483cf1
unknown <mikexross@gmail.com>
2018-02-25 19:05:45 -0700

    connection working

---
+++ config/orm.js
@@ -0,0 +1,40 @@
+
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(tableInput, callback) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    insertOne: function(table, cols, vals, callback) {
+        var queryString = "INSERT INTO " + table + " (burger_name) VALUES (?)";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    updateOne: function(table, objColVals, condition, callback) {
+        var queryString = "UPDATE " + table + " SET devoured=true WHERE " + condition;
+
+        console.log(queryString);
+        
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    }
+};
+// Export the ORM object in module.exports.
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ("Rodeo Burger");
+INSERT INTO burgers (burger_name) VALUES ("Mac and Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Elk Burger");
